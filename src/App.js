61d234bafde8c3a171484d3722079faa7f03539e
PavanKumar-bollam <bollampavan2909@gmail.com>
2023-10-20 17:17:22 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Table from './components/Table'
+import './App.css'
+
+import {
+  MainContainer,
+  CardContainer,
+  Heading,
+  ...
+} from './components/Table/styledComponents' /*update here*/
+// Replace your code here
+const App = () => <Table />
+
+export default App
